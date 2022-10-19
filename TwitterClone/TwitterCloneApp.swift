f789f4698e5a7e3a81ff6a744105f79b9792b1bb
SpaceColony <jovfernando@yahoo.com>
2022-10-19 16:13:16 +0700

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Johanes Vincent Fernando on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
